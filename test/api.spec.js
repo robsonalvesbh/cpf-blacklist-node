48425217ef85468b6dae9908e84a24e67e5a7a38
Robson Alves <robsonalves.dev@gmail.com>
2018-02-04 13:02:31 -0200

    alterado os testes para validar as alterações na api para trabalhar com cpf com ou sem formato.

--- test/api.spec.js
+++ test/api.spec.js
@@ -7,18 +7,32 @@ chai.use(chaiHttp);
 const { request } = chai;
 const constants = require('./../app/config/constants');
 
+const cpf = {
+  free: { cpf: constants.CPF_TEST_FREE },
+  freeWithoutFormat: { cpf: constants.CPF_TEST_FREE_WITHOUT_FORMAT },
+  block: { cpf: constants.CPF_TEST_BLOCK },
+  blockWithoutFormat: { cpf: constants.CPF_TEST_BLOCK_WITHOUT_FORMAT },
+};
+
 /**
  * Adiciona CPF a blocklist
  */
 describe('Route POST /api/v1/cpf', () => {
   it('Should add a cpf on blacklist', (done) => {
-    const cpf = {
-      cpf: constants.CPF_TEST,
-    };
-
     request(constants.ENDPOINT_API_V1)
       .post('/cpf')
-      .send(cpf)
+      .send(cpf.free)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_201);
+        expect(res.body.msg).to.eql(constants.MSG_CPF_ADDED_BLACKLIST);
+        done();
+      });
+  });
+
+  it('Should add a cpf on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .post('/cpf')
+      .send(cpf.freeWithoutFormat)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_201);
         expect(res.body.msg).to.eql(constants.MSG_CPF_ADDED_BLACKLIST);
@@ -32,13 +46,19 @@ describe('Route POST /api/v1/cpf', () => {
  */
 describe('Route POST /api/v1/cpf', () => {
   it('Should add a cpf on blacklist', (done) => {
-    const cpf = {
-      cpf: constants.CPF_TEST,
-    };
-
     request(constants.ENDPOINT_API_V1)
       .post('/cpf')
-      .send(cpf)
+      .send(cpf.free)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_500);
+        done();
+      });
+  });
+
+  it('Should add a cpf on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .post('/cpf')
+      .send(cpf.freeWithoutFormat)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_500);
         done();
@@ -66,7 +86,17 @@ describe('Route GET /api/v1/cpf', () => {
 describe('Route GET /api/v1/cpf/:cpf', () => {
   it('Should return a cpf that is on blacklist', (done) => {
     request(constants.ENDPOINT_API_V1)
-      .get(`/cpf/${constants.CPF_TEST}`)
+      .get(`/cpf/${cpf.free.cpf}`)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_200);
+        expect(res.body.cpf).to.eql(constants.MSG_BLOCK);
+        done();
+      });
+  });
+
+  it('Should return a cpf that is on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .get(`/cpf/${cpf.freeWithoutFormat.cpf}`)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_200);
         expect(res.body.cpf).to.eql(constants.MSG_BLOCK);
@@ -81,7 +111,17 @@ describe('Route GET /api/v1/cpf/:cpf', () => {
 describe('Route GET /api/v1/cpf/:cpf', () => {
   it('Should return a cpf that dont is on blacklist', (done) => {
     request(constants.ENDPOINT_API_V1)
-      .get(`/cpf/${constants.CPF_TEST_FAKE}`)
+      .get(`/cpf/${cpf.block.cpf}`)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_404);
+        expect(res.body.cpf).to.eql(constants.MSG_FREE);
+        done();
+      });
+  });
+
+  it('Should return a cpf that dont is on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .get(`/cpf/${cpf.blockWithoutFormat.cpf}`)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_404);
         expect(res.body.cpf).to.eql(constants.MSG_FREE);
@@ -96,7 +136,17 @@ describe('Route GET /api/v1/cpf/:cpf', () => {
 describe('Route DELETE /api/v1/cpf/:cpf', () => {
   it('Should remove a cpf on blacklist', (done) => {
     request(constants.ENDPOINT_API_V1)
-      .delete(`/cpf/${constants.CPF_TEST}`)
+      .delete(`/cpf/${cpf.free.cpf}`)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_200);
+        expect(res.body.msg).to.eql(constants.MSG_CPF_REMOVED_BLACKLIST);
+        done();
+      });
+  });
+
+  it('Should remove a cpf on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .delete(`/cpf/${cpf.freeWithoutFormat.cpf}`)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_200);
         expect(res.body.msg).to.eql(constants.MSG_CPF_REMOVED_BLACKLIST);
@@ -111,7 +161,17 @@ describe('Route DELETE /api/v1/cpf/:cpf', () => {
 describe('Route DELETE /api/v1/cpf/:cpf', () => {
   it('Should return error a cpf on blacklist', (done) => {
     request(constants.ENDPOINT_API_V1)
-      .delete(`/cpf/${constants.CPF_TEST_FAKE}`)
+      .delete(`/cpf/${cpf.block.cpf}`)
+      .end((err, res) => {
+        expect(res.status).to.eql(constants.STATUS_404);
+        expect(res.body.msg).to.eql(constants.MSG_CPF_NOT_FOUND_ON_BLACKLIST);
+        done();
+      });
+  });
+
+  it('Should return error a cpf on blacklist', (done) => {
+    request(constants.ENDPOINT_API_V1)
+      .delete(`/cpf/${cpf.blockWithoutFormat.cpf}`)
       .end((err, res) => {
         expect(res.status).to.eql(constants.STATUS_404);
         expect(res.body.msg).to.eql(constants.MSG_CPF_NOT_FOUND_ON_BLACKLIST);
